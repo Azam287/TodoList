5476faafb8a53be4a930def3d9be59f31f2dd7b3
Azam287 <mohdazamnoor28@gmail.com>
2021-05-29 02:51:25 +0530

    Adding filters and local storage

--- src/components/appcomp/App.jsx
+++ src/components/appcomp/App.jsx
@@ -1,13 +1,56 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import './App.css';
 import Form from '../Form';
 import TodoList from '../TodoList'
 
 function App() {
-
+  // States
   const [inputText, setInputText] = useState('')
   const [todos, setTodos] =useState([])
+  const[status, setStatus] = useState("all")
+  const [filteredTodos, setFilteredTodos]=useState([])
 
+  //Local Storage
+  const saveLocalTodos=()=>{
+      localStorage.setItem('todos', JSON.stringify(todos))
+  }
+
+  const getLocalTodos=()=>{
+    if(localStorage.getItem('todos')===null){
+      localStorage.setItem('todos', JSON.stringify([]))
+    }
+    else{
+     let todoLocal = JSON.parse(localStorage.getItem('todos'))
+     console.log(todoLocal)
+    }
+  }
+
+  //Run once
+  useEffect(()=>{
+    getLocalTodos()
+  },[])
+
+  useEffect(()=>{
+    filterHandler()
+    saveLocalTodos()
+  },[todos,status])
+
+  //Functions
+  const filterHandler = () => {
+    switch(status){
+      case 'completed':
+        setFilteredTodos(todos.filter(todo => todo.completed ===true))
+      break;
+
+      case 'uncompleted':
+        setFilteredTodos(todos.filter(todo => todo.completed ===false))
+        break; 
+
+      default:
+        setFilteredTodos(todos)
+        break;
+    }
+  }
 
   return (
     <div className="App">
@@ -15,8 +58,14 @@ function App() {
         <h1>To Do List</h1>
       </header>
 
-      <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText} />
-      <TodoList setTodos={setTodos} todos={todos} />
+      <Form 
+        inputText={inputText} 
+        todos={todos} 
+        setTodos={setTodos} 
+        setInputText={setInputText}
+        setStatus={setStatus} 
+      />
+      <TodoList setTodos={setTodos} todos={todos} filteredTodos={filteredTodos} />
     </div>
   );
 }
