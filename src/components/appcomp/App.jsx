143dc789f79d328b539edbbf078a9a310fa68991
Azam287 <mohdazamnoor28@gmail.com>
2021-05-29 01:17:01 +0530

    First commit

---
+++ src/components/appcomp/App.jsx
@@ -0,0 +1,24 @@
+import React, {useState} from 'react'
+import './App.css';
+import Form from '../Form';
+import TodoList from '../TodoList'
+
+function App() {
+
+  const [inputText, setInputText] = useState('')
+  const [todos, setTodos] =useState([])
+
+
+  return (
+    <div className="App">
+      <header>
+        <h1>To Do List</h1>
+      </header>
+
+      <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText} />
+      <TodoList setTodos={setTodos} todos={todos} />
+    </div>
+  );
+}
+
+export default App;
